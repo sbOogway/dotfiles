8e649fc6da87ed439cb92fb5e6ae05ae22eee3a2
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2026-01-31 20:39:57 +0100

    refactor: move each computer to its own branch

---
+++ home/.config/fish/functions/gits.fish
@@ -0,0 +1,4 @@
+function gits --wraps='git status' --description 'alias gits git status'
+  git status $argv
+        
+end
---
+++ home/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      files = {
+        hidden = true,
+      },
+    },
+    picker = {
+      files = {
+        hidden = true,
+      },
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
