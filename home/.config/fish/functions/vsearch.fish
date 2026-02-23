95c35b50ed91526f39ab80359db36d389851f898
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2026-02-23 12:41:11 +0100

    feat: add fuzzy vsearch function

---
+++ home/.config/fish/functions/vsearch.fish
@@ -0,0 +1,7 @@
+function vsearch
+    rg --line-number --column --no-heading --color=always --smart-case "$argv" | \
+    fzf --ansi \
+        --delimiter : \
+        --preview 'bat --style=numbers --color=always --highlight-line {2} {1} 2>/dev/null || cat {1}' \
+        --bind "enter:become(code --goto {1}:{2})"
+end
