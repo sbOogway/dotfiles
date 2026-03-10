dee51b3dedf317f17b4d25f562c648cefb3c6f7e
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2026-03-10 12:03:51 +0100

    chore: reformat

--- home/.config/fish/functions/vsearch.fish
+++ home/.config/fish/functions/vsearch.fish
@@ -1,6 +1,5 @@
 function vsearch
-    rg --line-number --column --no-heading --color=always --smart-case "$argv" | \
-    fzf --ansi \
+    rg --line-number --column --no-heading --color=always --smart-case "$argv" | fzf --ansi \
         --delimiter : \
         --preview 'bat --style=numbers --color=always --highlight-line {2} {1} 2>/dev/null || cat {1}' \
         --bind "enter:become(code --goto {1}:{2})"
